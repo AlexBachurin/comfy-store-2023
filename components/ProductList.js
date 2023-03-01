d4eeae42afea6021df796d3c0f82103bb3b7472d
Aleksandr <alexbachurin91@gmail.com>
2023-03-01 20:36:50 +0300

    display products in LIST VIEW

--- components/ProductList.js
+++ components/ProductList.js
@@ -5,7 +5,11 @@ import ListView from "./ListView";
 
 const ProductList = () => {
   const { filtered_products } = useFilterContext();
-  return <GridView products={filtered_products}></GridView>;
+  if (filtered_products.length < 1) {
+    return <h5>Sorry, no products matched your criteria</h5>;
+  }
+  // return <GridView products={filtered_products}></GridView>;
+  return <ListView products={filtered_products}></ListView>;
 };
 
 export default ProductList;
